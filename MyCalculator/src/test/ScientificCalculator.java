eda520fcf7be1adcea760cb2a64ea8962e93d36a
vnod <vnodgrg4@yahoo.com>
2013-04-10 13:51:32 +0100

    added files to branches

---
+++ MyCalculator/src/test/ScientificCalculator.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ScientificCalculator {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
